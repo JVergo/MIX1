b0cef66da17c8caf7cffd06179097a1444839fa7
Jvergo <Jvergoa@gmail.com>
2020-09-24 13:17:52 +0200

    First commit

---
+++ Assets/BoradeTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoradeTile : MonoBehaviour
+{
+    public Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.GetComponent<Renderer>().material.color = color;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
